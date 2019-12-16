0f5a5ece8cd554b5fd5a79192f3d0231d97640d6
snyarde <57542514+snyarde@users.noreply.github.com>
2019-12-16 15:11:50 -0500

    Completes Episode 4
    Completes episode 4: [Learn React & Material UI - #4 Lists, Tabs & Typography](https://www.youtube.com/watch?v=nF9q_fRV-1A)

--- src/Components/Exercises/index.js
+++ src/Components/Exercises/index.js
@@ -22,31 +22,42 @@ function ListItemLink(props) {
   return <ListItem button component="a" {...props} />;
 }
 
-export default ({ exercises }) => (
+export default ({
+  exercises,
+  category,
+  onSelect,
+  exercise: {
+    id,
+    title = "Welcome!",
+    description = "Please select an exercise from the list on the left."
+  }
+}) => (
   <Grid container>
     <Grid item sm>
       <Paper style={styles.Paper}>
-        {exercises.map(([group, exercises]) => (
-          <Fragment>
-            <Typography variant="h6" style={{ textTransform: "capitalize" }}>
-              {group}
-            </Typography>
-            <List component="ul">
-              {exercises.map(({ title }) => (
-                <ListItem button>
-                  <ListItemText primary={title} />
-                </ListItem>
-              ))}
-            </List>
-          </Fragment>
-        ))}
+        {exercises.map(([group, exercises]) =>
+          !category || category === group ? (
+            <Fragment key={group}>
+              <Typography variant="h6" style={{ textTransform: "capitalize" }}>
+                {group}
+              </Typography>
+              <List component="ul">
+                {exercises.map(({ id, title }) => (
+                  <ListItem key={id} button onClick={() => onSelect(id)}>
+                    <ListItemText primary={title} />
+                  </ListItem>
+                ))}
+              </List>
+            </Fragment>
+          ) : null
+        )}
       </Paper>
     </Grid>
     <Grid item sm>
       <Paper style={styles.Paper}>
-        <Typography variant="h4">Welcome:</Typography>
+        <Typography variant="h4">{title}</Typography>
         <Typography variant="body1" style={{ marginTop: 20 }}>
-          Please select an exercise from the list on the left.
+          {description}
         </Typography>
       </Paper>
     </Grid>
