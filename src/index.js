2ac654729a4e0fbd87b99162dda60bed9dc0b89d
snyarde <57542514+snyarde@users.noreply.github.com>
2019-12-16 09:38:35 -0500

    Completes Episode 1
    Completes episode 1: [Learn React & Material UI - #1 Intro](https://www.youtube.com/watch?v=xm4LX5fJKZ8)

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-import "./styles.css";
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Snyarde</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
-}
+import App from "./Components/App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
