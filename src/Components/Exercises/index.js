9bde9b6eae1d8afa8d829639c180850cd2f8a4e0
snyarde <57542514+snyarde@users.noreply.github.com>
2019-12-16 12:48:04 -0500

    Completes Episode 3
    Completes episode 3: [Learn React & Material UI - #3 Data Store](https://www.youtube.com/watch?v=fox4Y-_Q9hE)

--- src/Components/Exercises/index.js
+++ src/Components/Exercises/index.js
@@ -1,3 +1,54 @@
-import React from "react";
+import React, { Fragment } from "react";
+import {
+  Grid,
+  Paper,
+  Typography,
+  List,
+  ListItem,
+  ListItemText
+} from "@material-ui/core";
 
-export default props => <div>Hello from Exercises</div>;
+const styles = {
+  Paper: {
+    padding: 20,
+    marginTop: 10,
+    marginBottom: 10,
+    height: 500,
+    overflowY: "auto"
+  }
+};
+
+function ListItemLink(props) {
+  return <ListItem button component="a" {...props} />;
+}
+
+export default ({ exercises }) => (
+  <Grid container>
+    <Grid item sm>
+      <Paper style={styles.Paper}>
+        {exercises.map(([group, exercises]) => (
+          <Fragment>
+            <Typography variant="h6" style={{ textTransform: "capitalize" }}>
+              {group}
+            </Typography>
+            <List component="ul">
+              {exercises.map(({ title }) => (
+                <ListItem button>
+                  <ListItemText primary={title} />
+                </ListItem>
+              ))}
+            </List>
+          </Fragment>
+        ))}
+      </Paper>
+    </Grid>
+    <Grid item sm>
+      <Paper style={styles.Paper}>
+        <Typography variant="h4">Welcome:</Typography>
+        <Typography variant="body1" style={{ marginTop: 20 }}>
+          Please select an exercise from the list on the left.
+        </Typography>
+      </Paper>
+    </Grid>
+  </Grid>
+);
